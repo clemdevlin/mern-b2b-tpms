20c0e307c634b8f261c373ccc95c4311d56f3a12
Clem Devlin <egyayemi2006@gmail.com>
2025-11-18 02:38:41 +0000

    Change session secure and sameSite settings

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -39,9 +39,9 @@ app.use(
     name: "session",
     keys: [config.SESSION_SECRET],
     maxAge: 24 * 60 * 60 * 1000,
-    secure: config.NODE_ENV === "production",
+    secure: true,
     httpOnly: true,
-    sameSite: "lax",
+    sameSite: "none",
   })
 );
 
