50fa249ecb6d2356cf97e5eba92c49e333fd3aa4
Clement <egyayemi2006@gmail.com>
2026-03-10 06:55:44 +0000

    Update session configuration for production environment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -33,15 +33,15 @@ app.use(
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: true }));
-
+app.set("trust proxy", 1);
 app.use(
   session({
     name: "session",
     keys: [config.SESSION_SECRET],
     maxAge: 24 * 60 * 60 * 1000,
-    secure: true,
+    secure: config.NODE_ENV === "production",
     httpOnly: true,
-    sameSite: "none",
+    sameSite: config.NODE_ENV === "production" ? "none" : "lax",
   })
 );
 
